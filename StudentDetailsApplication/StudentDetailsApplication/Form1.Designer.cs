2acd1b2a32bd84c2b6a137c39b219dec98156678
muhammed uvais <36261926+uvaisashraf@users.noreply.github.com>
2021-12-27 16:50:31 +0530

    Add files via upload

---
+++ StudentDetailsApplication/StudentDetailsApplication/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace StudentDetailsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbStudentID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbStudentName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbStudentCountry = new System.Windows.Forms.TextBox();
+            this.tbStudentCity = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbStudentAge = new System.Windows.Forms.TextBox();
+            this.tbStudentCourse = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentCourse = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentCountry = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StudentCity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbStudentID
+            // 
+            this.tbStudentID.Location = new System.Drawing.Point(112, 94);
+            this.tbStudentID.Name = "tbStudentID";
+            this.tbStudentID.Size = new System.Drawing.Size(250, 20);
+            this.tbStudentID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student ID";
+            // 
+            // tbStudentName
+            // 
+            this.tbStudentName.Location = new System.Drawing.Point(112, 136);
+            this.tbStudentName.Name = "tbStudentName";
+            this.tbStudentName.Size = new System.Drawing.Size(250, 20);
+            this.tbStudentName.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // tbStudentCountry
+            // 
+            this.tbStudentCountry.Location = new System.Drawing.Point(112, 282);
+            this.tbStudentCountry.Name = "tbStudentCountry";
+            this.tbStudentCountry.Size = new System.Drawing.Size(250, 20);
+            this.tbStudentCountry.TabIndex = 0;
+            // 
+            // tbStudentCity
+            // 
+            this.tbStudentCity.Location = new System.Drawing.Point(112, 324);
+            this.tbStudentCity.Name = "tbStudentCity";
+            this.tbStudentCity.Size = new System.Drawing.Size(250, 20);
+            this.tbStudentCity.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Student Country";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Student City";
+            // 
+            // tbStudentAge
+            // 
+            this.tbStudentAge.Location = new System.Drawing.Point(112, 189);
+            this.tbStudentAge.Name = "tbStudentAge";
+            this.tbStudentAge.Size = new System.Drawing.Size(250, 20);
+            this.tbStudentAge.TabIndex = 0;
+            // 
+            // tbStudentCourse
+            // 
+            this.tbStudentCourse.Location = new System.Drawing.Point(112, 231);
+            this.tbStudentCourse.Name = "tbStudentCourse";
+            this.tbStudentCourse.Size = new System.Drawing.Size(250, 20);
+            this.tbStudentCourse.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Student Age";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Student Course";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(215, 370);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(43, 370);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(296, 370);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(134, 370);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToDeleteRows = false;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.StudentName,
+            this.StudentAge,
+            this.StudentCourse,
+            this.StudentCountry,
+            this.StudentCity});
+            this.dgv1.Location = new System.Drawing.Point(404, 81);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.Size = new System.Drawing.Size(356, 329);
+            this.dgv1.TabIndex = 3;
+            this.dgv1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellDoubleClick);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Visible = false;
+            // 
+            // StudentName
+            // 
+            this.StudentName.DataPropertyName = "StudentName";
+            this.StudentName.HeaderText = "Student Name";
+            this.StudentName.Name = "StudentName";
+            this.StudentName.ReadOnly = true;
+            // 
+            // StudentAge
+            // 
+            this.StudentAge.DataPropertyName = "StudentAge";
+            this.StudentAge.HeaderText = "Student Age";
+            this.StudentAge.Name = "StudentAge";
+            this.StudentAge.ReadOnly = true;
+            // 
+            // StudentCourse
+            // 
+            this.StudentCourse.DataPropertyName = "StudentCourse";
+            this.StudentCourse.HeaderText = "Student Course";
+            this.StudentCourse.Name = "StudentCourse";
+            this.StudentCourse.ReadOnly = true;
+            // 
+            // StudentCountry
+            // 
+            this.StudentCountry.DataPropertyName = "StudentCountry";
+            this.StudentCountry.HeaderText = "Student Country";
+            this.StudentCountry.Name = "StudentCountry";
+            this.StudentCountry.ReadOnly = true;
+            // 
+            // StudentCity
+            // 
+            this.StudentCity.DataPropertyName = "StudentCity";
+            this.StudentCity.HeaderText = "Student City";
+            this.StudentCity.Name = "StudentCity";
+            this.StudentCity.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 430);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbStudentCourse);
+            this.Controls.Add(this.tbStudentAge);
+            this.Controls.Add(this.tbStudentCity);
+            this.Controls.Add(this.tbStudentCountry);
+            this.Controls.Add(this.tbStudentName);
+            this.Controls.Add(this.tbStudentID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbStudentID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbStudentName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbStudentCountry;
+        private System.Windows.Forms.TextBox tbStudentCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbStudentAge;
+        private System.Windows.Forms.TextBox tbStudentCourse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentAge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentCourse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentCountry;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StudentCity;
+    }
+}
+
---
+++ StudentDetailsApplication/StudentDetailsApplication/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentDetailsApplication
+{
+    public partial class Form1 : Form
+    {
+        STDTBL stdmodel = new STDTBL();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbStudentID.Clear();
+            tbStudentName.Clear();
+            tbStudentAge.Clear();
+            tbStudentCourse.Clear();
+            tbStudentCity.Clear();
+            tbStudentCountry.Clear();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            stdmodel.StudentName = tbStudentName.Text;
+            stdmodel.StudentAge = int.Parse(tbStudentAge.Text);
+            stdmodel.StudentCourse = tbStudentCourse.Text;
+            stdmodel.StudentCity = tbStudentCity.Text;
+            stdmodel.StudentCountry = tbStudentCountry.Text;
+            
+
+
+            using (StudentDetaiIsEntities db = new StudentDetaiIsEntities())
+            {
+                db.STDTBLs.Add(stdmodel);
+                db.SaveChanges();
+                dgv1.DataSource = db.STDTBLs.ToList();
+            }
+            MessageBox.Show("submitted successfully");
+            tbStudentID.Clear();
+            tbStudentName.Clear();
+            tbStudentAge.Clear();
+            tbStudentCourse.Clear();
+            tbStudentCity.Clear();
+            tbStudentCountry.Clear();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (StudentDetaiIsEntities db = new StudentDetaiIsEntities())
+            {
+                
+                dgv1.DataSource = db.STDTBLs.ToList();
+            }
+        }
+
+        private void dgv1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowindex = e.RowIndex;
+            tbStudentID.Text = dgv1.Rows[rowindex].Cells[0].Value.ToString();
+            tbStudentName.Text = dgv1.Rows[rowindex].Cells[1].Value.ToString();
+            tbStudentAge.Text = dgv1.Rows[rowindex].Cells[2].Value.ToString();
+            tbStudentCourse.Text = dgv1.Rows[rowindex].Cells[3].Value.ToString();
+            tbStudentCountry.Text = dgv1.Rows[rowindex].Cells[4].Value.ToString();
+            tbStudentCity.Text = dgv1.Rows[rowindex].Cells[5].Value.ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            stdmodel.Id = Convert.ToInt32(dgv1.CurrentRow.Cells["Id"].Value);
+            using(StudentDetaiIsEntities db =new StudentDetaiIsEntities())
+            {
+                stdmodel = db.STDTBLs.Where(x => x.Id == stdmodel.Id).FirstOrDefault();
+                db.STDTBLs.Remove(stdmodel);
+                db.SaveChanges();
+                dgv1.DataSource = db.STDTBLs.ToList();
+                tbStudentID.Clear();
+                tbStudentName.Clear();
+                tbStudentAge.Clear();
+                tbStudentCourse.Clear();
+                tbStudentCity.Clear();
+                tbStudentCountry.Clear();
+            }
+               
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+            using (StudentDetaiIsEntities db = new StudentDetaiIsEntities())
+            {
+                stdmodel.Id = Convert.ToInt32(dgv1.CurrentRow.Cells["Id"].Value);
+                stdmodel.StudentName = tbStudentName.Text;
+                stdmodel.StudentAge = int.Parse(tbStudentAge.Text);
+                stdmodel.StudentCourse = tbStudentCourse.Text;
+                stdmodel.StudentCity = tbStudentCity.Text;
+                stdmodel.StudentCountry = tbStudentCountry.Text;
+                db.STDTBLs.Attach(stdmodel);
+                db.SaveChanges();
+                dgv1.DataSource = db.STDTBLs.ToList();
+                tbStudentID.Clear();
+                tbStudentName.Clear();
+                tbStudentAge.Clear();
+                tbStudentCourse.Clear();
+                tbStudentCity.Clear();
+                tbStudentCountry.Clear();
+
+            }
+
+        }
+    }
+}
